300c4f9e91d81c13151504434fa9dfbe8231dd27
Boska40 <stephaneboska@gmail.com>
2023-11-02 12:05:56 +0100

    ok

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>CSS List Item Hover Effects Html CSS Tutorial For Beginners</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="box">
+        <h2>List Item Hover Effects</h2>
+        <ul>
+            <li><span>1</span>List of Item One</li>
+            <li><span>2</span>List of Item two</li>
+            <li><span>3</span>List of Item three</li>
+            <li><span>4</span>List of Item four</li>
+            <li><span>5</span>List of Item five</li>
+            <li><span>6</span>List of Item six</li>
+        </ul>
+    </div>
+</html>
